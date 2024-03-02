7e3bac0161a3c2e8ece9a655ea23bbb4d4f77bb1
jamal1608 <ahmedbhatti909@gmail.com>
2024-03-02 11:42:37 +0500

    divisble by 3

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var userNumber = parseInt(prompt("Enter a number:"));
+
+if (userNumber % 3 === 0) {
+    alert(userNumber + " is divisible by 3")
+} else {
+    alert(userNumber + " is not divisible by 3");
+}
